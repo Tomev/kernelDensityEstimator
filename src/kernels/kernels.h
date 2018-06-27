1e901de1c96f508617b65897733ca04e55db90d1
Tomasz Rybotycki <rybotycki.tomasz@gmail.com>
2018-06-27 11:46:17 +0200

    [STABLE] Kernels.h file added, containing all kernels headers.

---
+++ src/kernels/kernels.h
@@ -0,0 +1,9 @@
+#ifndef KERNELDENSITYESTIMATOR_KERNELS_H
+#define KERNELDENSITYESTIMATOR_KERNELS_H
+
+#include "epanecznikowKernel.h"
+#include "linearKernel.h"
+#include "normalKernel.h"
+#include "triangularKernel.h"
+
+#endif //KERNELDENSITYESTIMATOR_KERNELS_H
